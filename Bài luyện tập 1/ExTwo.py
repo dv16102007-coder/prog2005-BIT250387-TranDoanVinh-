5276d6c532bb27cd1db093473cea570127b54409
dv16102007-coder <dv16102007@gmail.com>
2026-03-13 17:42:16 +0700

    Update ExTwo.py

--- Bài luyện tập 1/ExTwo.py
+++ Bài luyện tập 1/ExTwo.py
@@ -1,1 +1,7 @@
-
+def bai_tap2 ():
+ import math
+ x = int(input("Nhập số thứ nhất"))
+ y = int(input("Nhập số thứ hai"))
+ print("Lũy thừa:",x**y)
+ print("Căn bậc 2 của x:", math.sqrt(x))
+ print("Chia lấy phần dư:",x//y)
