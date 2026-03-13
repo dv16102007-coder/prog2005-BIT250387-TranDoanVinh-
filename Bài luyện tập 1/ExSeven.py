6c695e88cd360d4da8481871401ec2e8db9e1164
dv16102007-coder <dv16102007@gmail.com>
2026-03-13 17:47:34 +0700

    Create ExSeven.py

---
+++ Bài luyện tập 1/ExSeven.py
@@ -0,0 +1,9 @@
+def bai_tap7 ():
+ class Student:
+  def __init__(self, ten, diem):
+   self.ten = ten
+   self.diem = diem
+ sv1= Student("Vinh",10)
+ sv2= Student("Minh",0)
+ print(sv1.ten, sv1.diem)
+ print(sv2.ten, sv2.diem)
